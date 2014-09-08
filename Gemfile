ae90d830c7c87e37f8f0ec0b6c825df7df974bbe
Ricardo <ricardocdzg@hotmail.com>
2014-09-08 15:32:17 -0300

    adjusting some details on the view

--- Gemfile
+++ Gemfile
@@ -24,6 +24,10 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
 
+gem "therubyracer"
+
+gem "less-rails"
+
 gem 'bootstrap-datepicker-rails'
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',        group: :development
--- app/models/evaluation_period.rb
+++ app/models/evaluation_period.rb
@@ -1,2 +1,22 @@
 class EvaluationPeriod < ActiveRecord::Base
+	 validates_presence_of :start_date_evaluation , :end_date_evaluation
+
+	 validate :dates_are_valid
+
+
+  def dates_are_valid
+    if self.end_date_evaluation.present? && self.start_date_evaluation.present?
+
+		start_date_evaluation = DateTime.strptime(self.start_date_evaluation, "%d/%m/%Y")
+		end_date_evaluation = DateTime.strptime("#{self.end_date_evaluation} 23:59:59", "%d/%m/%Y %H:%M:%S")
+
+	    if end_date_evaluation < start_date_evaluation
+	    	errors.add(:end_date_evaluation, "A data final deve ser após a data de início do intervalo.")
+	    end
+		else
+			
+		end	
+  	end
+
+
 end
