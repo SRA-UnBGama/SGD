6983caded9ba7ba051d918b2ae78d5882dea0163
TomazMartins <tomaz.unb@outlook.com>
2014-09-17 09:47:26 -0300

    Validating the longer term that the date today

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,4 +2,17 @@ class Goal < ActiveRecord::Base
 	has_and_belongs_to_many :forms
 
 	validates_presence_of :name_goal, :deadline_goal, :description_goal
+
+	validate :deadline_greater_than_today
+
+	def deadline_greater_than_today
+	  today = Date.today.to_formatted_s(:db)
+	  deadline = self.deadline_goal.to_formatted_s(:db)
+
+	  if deadline < today
+		errors.add(:deadline_goal, "O prazo deve ser após a data de hoje.")
+	  else
+			#Nothing to do
+	  end
+	end
 end
