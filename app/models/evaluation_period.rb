e1a3d0600ba4092cd93f24f59a42cc4242cb460a
RIcardo <ricardocdzg@hotmail.com>
2014-09-29 14:47:55 -0300

    Internationalization

--- app/models/evaluation_period.rb
+++ app/models/evaluation_period.rb
@@ -17,11 +17,13 @@ class EvaluationPeriod < ActiveRecord::Base
   end
 
   def minimum_period
+  	if self.end_date_evaluation.present? && self.start_date_evaluation.present?
 
-	if end_date_evaluation < (start_date_evaluation + 59.days)
-		errors.add(:end_date_evaluation, "Período deve ser superior à 60 dias")
-	else
-		#Nothing to do
+		if end_date_evaluation < (start_date_evaluation + 59.days)
+			errors.add(:end_date_evaluation, "Período deve ser superior à 60 dias")
+		else
+			#Nothing to do
+		end
 	end
   end
   	
