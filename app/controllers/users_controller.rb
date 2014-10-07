bdf0f3937efe9e2bdee678b43f6a21493ddf0bed
Thaiane <thaiane.unb@hotmail.com>
2014-10-07 14:47:57 -0300

    Deleting user's show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,8 +29,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to users_path, notice: 'User was successfully created.' }
+        format.json { render :index, status: :created, location: @user }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -43,8 +43,8 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+        format.json { render :index, status: :ok, location: @user }
       else
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
