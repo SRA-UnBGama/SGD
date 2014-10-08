8c63b6c0b126b053795a4336222603049afef66b
Thaiane <thaiane.unb@hotmail.com>
2014-10-08 11:09:02 -0300

    Validation about limit of working condition

--- app/controllers/working_conditions_controller.rb
+++ app/controllers/working_conditions_controller.rb
@@ -20,15 +20,21 @@ class WorkingConditionsController < ApplicationController
   # POST /working_conditions
   # POST /working_conditions.json
   def create
+
+    workings_conditions_actived = WorkingCondition.where(:is_active_condition => true).count
     @working_condition = WorkingCondition.new(working_condition_params)
     @working_condition.is_active_condition = true
 
     respond_to do |format|
-      if @working_condition.save
-        format.html { redirect_to working_conditions_path, notice: 'Working condition was successfully created.' }
+      if workings_conditions_actived < 10
+        if @working_condition.save
+         format.html { redirect_to working_conditions_path, notice: 'Working condition was successfully created.' }
+        else
+          format.html { render :new }
+          format.json { render json: @working_condition.errors, status: :unprocessable_entity }
+        end
       else
-        format.html { render :new }
-        format.json { render json: @working_condition.errors, status: :unprocessable_entity }
+        format.html { redirect_to working_conditions_path, notice: "Já existem 10 condições de trabalho ativas. Desative alguma antes de cadastrar uma nova condição de trabalho." }
       end
     end
   end
@@ -50,12 +56,17 @@ class WorkingConditionsController < ApplicationController
   # DELETE /working_conditions/1.json
   def destroy
     @working_condition.is_active_condition ? @working_condition.is_active_condition = false : @working_condition.is_active_condition = true
-    @working_condition.save
-
 
     respond_to do |format|
-      format.html { redirect_to working_conditions_url, notice: 'Condição de Trabalho Desativada com Sucesso.' }
-      format.json { head :no_content }
+      workings_conditions_actived = WorkingCondition.where(:is_active_condition => true).count
+      if !(workings_conditions_actived >= 10 && (@working_condition.is_active_condition == true))
+        @working_condition.save
+        format.html { redirect_to working_conditions_url, notice: 'Condição de Trabalho alterada com sucesso.' }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to working_conditions_url, notice: 'Condição de Trabalho não alterada. Limite de Condições de Trabalho ativas foi Atingido.' }
+        format.json { head :no_content }
+      end
     end
   end
 
