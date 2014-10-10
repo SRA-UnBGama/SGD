f253f3a754116bf53179fc658d3d3f4e3119e9dd
MaxAlmeida <llewxam100@hotmail.com>
2014-10-10 17:31:02 -0300

    Validate max number of competences

--- app/models/competence.rb
+++ app/models/competence.rb
@@ -4,5 +4,16 @@ class Competence < ActiveRecord::Base
 #	validates_presence_of :type_competence
 #	validates_uniqueness_of :type_competence
 	validates_uniqueness_of :name_competence
+	validate :max_number_of_competence
 
+def max_number_of_competence
+	max = 3
+
+	if(Competence.count >= max)
+	errors.add( :name_competence,"O número de competências deve ser menor que 10.")
+	else
+		#nothing to do!!!!!!!!!!!!
+	end
+
+end
 end
