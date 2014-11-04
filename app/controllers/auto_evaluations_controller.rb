f0ef6b094a34ea49c00eed0ab9bf3cc2246dd8d3
Ricardogtx <ricardocdzg@hotmail.com>
2014-11-04 16:57:10 -0200

    implemeting evaluation for users

--- app/controllers/auto_evaluations_controller.rb
+++ app/controllers/auto_evaluations_controller.rb
@@ -1,10 +1,28 @@
 class AutoEvaluationsController < ApplicationController
 
 def index
-    @competences = Competence.all
-    @working_conditions = WorkingCondition.all
-    @goals = Goal.all
-end
+    @evaluation = Evaluation.create
+    @competences_fundamental = @evaluation.evaluation_items.where(:type => "Competence",:category => "Fundamental")
+    @competences_gerencial = @evaluation.evaluation_items.where(:category => "Gerencial", :type => "Competence")
+
+  end
+
+  def show
+    @evaluation = @evaluation.evaluation_items.where(:type => "Competence",:category => "Fundamental").find(params[:id])
+  end
 
 
+  def edit
+  end
+
+
+	def update
+	respond_to do |format|
+      if @competence.update(competence_params)
+        format.html { redirect_to auto_evaluation_path, notice: 'Competence was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+	end	
 end
