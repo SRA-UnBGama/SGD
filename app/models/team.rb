8857dc068a40be909ae37a3db8c18342a08bd5d0
TomazMartins <tomaz.unb@outlook.com>
2014-11-10 11:19:33 -0200

    Adding the buttom to user confirm the Team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,15 +1,15 @@
 class Team < ActiveRecord::Base
   has_many :users
+
+  def initialize_team
+    self.users = search_by_workplace
+  end
   
   def search_by_workplace
     place = self.workplace
     members = User.where("cost_center = ? ", place)
 
     members
-  end
-
-  def initialize_team
-    self.users = search_by_workplace
   end
 
   def list_name_members
