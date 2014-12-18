cf7d80e7e9f87e9e794c412fff00fbd3ff462aad
Daniel Henrique <danielhmarinho@gmail.com>
2014-12-18 17:50:16 -0200

    Second scenario and view changes

--- features/external_user.feature
+++ features/external_user.feature
@@ -14,3 +14,29 @@ Scenario: Login with external_user
 	Then I fill 'Senha:' field
 	Then I click on 'Logar' button
  	Then the index page is loaded
+
+Scenario: Create a new external_user
+  	Given I am on the index page
+  	When I click on 'Usuario' link
+  	Then the login page is loaded
+  	When I click on 'Cadastrar Novo Usuário' link
+  	Then the sign_up page is loaded
+  	Then I fill 'Nome:' field
+  	Then I fill 'Matrícula FUB:' field
+	Then I fill 'Matrícula SIAPE:' field
+  	Then I fill 'Ocupação:' field
+	Then I fill 'Lotação:' field
+  	Then I fill 'E-mail:' field
+  	Then I fill 'SenhaC:' field
+  	Then I fill 'Confirmação de Senha:' field
+
+
+
+
+
+
+
+
+
+
+
--- features/step_definitions/external_user_steps.rb
+++ features/step_definitions/external_user_steps.rb
@@ -25,3 +25,45 @@ end
 Then (/^the index page is loaded$/) do
 	visit 'http://localhost:3000/'
 end
+
+#Scenario Login with external_user
+
+When (/^I click on 'Cadastrar Novo Usuário' link$/) do
+	click_link('Cadastrar Novo Usuário')
+end
+
+Then (/^the sign_up page is loaded$/) do
+	visit 'http://localhost:3000/users/sign_up'
+end
+
+Then (/^I fill 'Nome:' field$/) do
+	fill_in 'Nome:', :with => 'Teste'
+end
+
+Then (/^I fill 'Matrícula FUB:'$/) do
+	fill_in 'Matrícula FUB:', :with => '12356632'
+end
+
+Then(/^I fill 'Matrícula SIAPE:' field$/) do
+	fill_in 'Matrícula SIAPE:', :with => '1235763732'
+end
+
+Then(/^I fill 'Ocupação:' field$/) do
+	fill_in 'Ocupação:', :with => 'Gestor'
+end
+
+Then(/^I fill 'Lotação:' field$/) do
+	fill_in 'Lotação:', :with => 'FGA'
+end
+
+Then(/^I fill 'E-mail:' field$/) do
+	fill_in 'E-mail:', :with => 'teste@teste.com'
+end
+
+Then (/^I fill 'SenhaC:' field$/) do
+	fill_in "Senha", with: "123456"
+end
+
+Then(/^I fill 'Confirmação de Senha:' field$/) do
+	fill_in 'Confirmação de Senha:', :with => '123456'
+end
