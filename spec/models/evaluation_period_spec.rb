729386707b024956a99b33376e5fd8931496d275
paulohtfs <paulohtfs@gmail.com>
2014-12-18 03:34:39 -0200

    Implementing more test to evaluation_period model class

--- spec/models/evaluation_period_spec.rb
+++ spec/models/evaluation_period_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe EvaluationPeriod, :type => :model do
+
 	CLOSED = "Encerrado"
 	IN_PROGRESS = "Em Andamento"
 	WAITING = "Aguardando"
 
+	let( :valid_period_attributes ){
+		{
+			:start_date_evaluation => "2014-01-01",
+			:end_date_evaluation => "2014-04-30"
+		}
+	}
+
 	subject( :period ) {
-		EvaluationPeriod.new( :start_date_evaluation => "2014-01-01", :end_date_evaluation => "2014-04-30" )
+		EvaluationPeriod.new( valid_period_attributes )
+	}
+
+	subject( :second_period ) {
+		EvaluationPeriod.new( :start_date_evaluation => "2015-01-01", :end_date_evaluation => "2015-04-30" )
 	}
 
 	subject( :planning_phase ) {
@@ -35,7 +47,11 @@ RSpec.describe EvaluationPeriod, :type => :model do
 		period.phases << development_plan_phase
 	end
 
-	describe "#define_status_phases" do
+	describe '#verify_status_phases' do
+		# Pendent
+	end
+
+	describe '#define_status_phases' do
 
 		it "assign status phases is closed" do
 			period.phases.first.start_date_phase = Date.today - 31
@@ -59,7 +75,7 @@ RSpec.describe EvaluationPeriod, :type => :model do
 			expect(phase_status).to eq(IN_PROGRESS)
 		end
 
-		it "assign status phases is in progress" do
+		it "assign status phases is in waiting" do
 			period.phases.first.start_date_phase = Date.today + 1
 			period.phases.first.end_date_phase = Date.today + 30
 
@@ -69,5 +85,71 @@ RSpec.describe EvaluationPeriod, :type => :model do
 
 			expect(phase_status).to eq(WAITING)
 		end
+
 	end
+
+	describe '#define_status_evaluation_period' do
+
+		it "assign evaluation status to closed" do
+			period.start_date_evaluation = Date.today - 1
+			period.end_date_evaluation = Date.today - 1
+
+			period.define_status_evaluation_period
+			status_result = period.status_evaluation_period
+			expect(status_result).to eq(CLOSED)
+		end
+
+		it "assign evaluation status to in progress" do
+			period.start_date_evaluation = Date.today - 1
+			period.end_date_evaluation = Date.today + 1
+
+			period.define_status_evaluation_period
+			status_result = period.status_evaluation_period
+			expect(status_result).to eq(IN_PROGRESS)
+		end
+
+		it "assign evaluation status to waiting" do
+			period.start_date_evaluation = Date.today + 1
+			period.end_date_evaluation = Date.today + 1
+
+			period.define_status_evaluation_period
+			status_result = period.status_evaluation_period
+			expect(status_result).to eq(WAITING)
+		end
+
+	end
+
+	describe '#dates_are_valid' do
+		# Pendent
+	end
+
+	describe '#minimum_period' do
+		# Pendent
+	end
+
+	describe '#not_overlap_evaluation_period' do
+
+		it "return nil" do
+		end
+
+	end
+
+	describe '#create_phases' do
+		# Pendent
+	end
+
+	describe '#initialize_phases' do
+		# Pendent
+	end
+
+	describe '#get_current_name_phase' do
+
+		it "should not have in progress phases" do
+			period.status_evaluation_period = nil
+			phase_name = EvaluationPeriod.get_current_name_phase
+			expect(phase_name).to include("Não há fase em andamento no momento.")
+		end
+
+	end
+
 end
